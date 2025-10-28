e5580d272ce6dbbf6075bf7347844fab81e56f5a
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2025-10-28 20:25:14 -0300

    Refactor HTTP server to implement a Router class and Trie structure for route handling

--- http/index.js
+++ http/index.js
@@ -2,58 +2,139 @@ const http = require('node:http')
 
 const PORT = 5255
 
-const routeHandlers = {
-  'GET /': () => ({
-    statusCode: 200,
-    data: 'Hello, World!',
-    headers: { 'My-Header': 'MyHeaderValue' }
-  }),
-  'POST /echo': () => ({
-    statusCode: 201,
-    data: 'Echoing your POST request!',
-    headers: { 'My-Header': 'MyHeaderValue' }
-  })
+const HTTP_METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  PATCH: 'PATCH',
+  HEAD: 'HEAD',
+  OPTIONS: 'OPTIONS',
+  CONNECT: 'CONNECT',
+  TRACE: 'TRACE'
 }
 
-const baseHeaders = {
-  'Content-Type': 'text/plain; charset=utf-8',
+class Trie {
+  constructor() {
+    this.root = new TrieNode()
+  }
+
+  insert(wordToInsert, node = this.root) {
+    let length = wordToInsert.length
+    if (length == 0) return
+
+    const letters = wordToInsert.split('')
+
+    const foundNode = node.children.get(wordToInsert[0])
+
+    if (foundNode) {
+      this.insert(letters.slice(1).join(''), foundNode)
+    } else {
+      let insertedNode = node.add(letters[0], length == 1)
+      this.insert(letters.slice(1).join(''), insertedNode)
+    }
+  }
+}
+
+class TrieNode {
+  constructor() {
+    this.isEndOfWord = false
+    this.children = new Map()
+  }
+
+  add(letter, _isLastCharacter) {
+    let newNode = new TrieNode()
+    this.children.set(letter, newNode)
+
+    if (_isLastCharacter) newNode.isEndOfWord = true
+    return newNode
+  }
+}
+
+class Router {
+  constructor() {
+    this.routes = new Map()
+  }
+
+  #addRoute(method, path, handler) {
+    if (typeof path !== 'string' || typeof handler !== 'function') {
+      throw new Error(
+        'Invalid argument types: path must be a string and handler must be a function'
+      )
+    }
+
+    this.routes.set(`${method} ${path}`, handler)
+  }
+
+  printRoutes() {
+    console.log('Registered routes: %o', this.routes)
+  }
+
+  handleRequest(req, res) {
+    const { url, method } = req
+    const handler = this.routes.get(`${method} ${url}`)
+
+    if (!handler) {
+      return console.log('404 not found')
+    }
+
+    handler(req, res)
+  }
+
+  get(path, handler) {
+    this.#addRoute(HTTP_METHODS.GET, path, handler)
+  }
+
+  post(path, handler) {
+    this.#addRoute(HTTP_METHODS.POST, path, handler)
+  }
+
+  put(path, handler) {
+    this.#addRoute(HTTP_METHODS.PUT, path, handler)
+  }
+
+  delete(path, handler) {
+    this.#addRoute(HTTP_METHODS.DELETE, path, handler)
+  }
+
+  patch(path, handler) {
+    this.#addRoute(HTTP_METHODS.PATCH, path, handler)
+  }
+
+  head(path, handler) {
+    this.#addRoute(HTTP_METHODS.HEAD, path, handler)
+  }
+
+  options(path, handler) {
+    this.#addRoute(HTTP_METHODS.OPTIONS, path, handler)
+  }
+
+  connect(path, handler) {
+    this.#addRoute(HTTP_METHODS.CONNECT, path, handler)
+  }
+
+  trace(path, handler) {
+    this.#addRoute(HTTP_METHODS.TRACE, path, handler)
+  }
+}
+
+const baseHeader = {
+  'Content-Type': 'application/json; charset=utf-8',
   Connection: 'close'
 }
 
-/**
- *
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- */
-function handleRequest(req, res) {
-  const { method, url } = req
-  const handler =
-    routeHandlers[`${method} ${url}`] ||
-    (() => ({
-      statusCode: 404,
-      data: 'Not Found',
-      headers: { 'My-Header': 'NotFound' }
-    }))
+const router = new Router()
 
-  const { statusCode, data, headers: headersFromHandler } = handler()
-
-  headers = {
-    ...baseHeaders,
-    ...headersFromHandler,
-    'Content-Length': Buffer.byteLength(data)
-  }
-
-  res.writeHead(statusCode, headers)
-
-  res.end(`
-    Request method: ${req.method}
-    Request URL: ${req.url}
-    Response headers: ${JSON.stringify(res.getHeaders(), null, 2)}
-  `)
-}
-
-const server = http.createServer(handleRequest)
-
-server.listen(PORT, () => {
-  console.log(`HTTP server is running on http://localhost:${PORT}`)
+router.get('/', function handleBaseGet(req, res) {
+  res.writeHead(200, baseHeader)
+  res.end(JSON.stringify({ message: 'Hello, World!' }))
 })
+
+router.get('/about', function handleAboutGet(req, res) {
+  res.writeHead(200, baseHeader)
+  res.end(JSON.stringify({ message: 'About Page' }))
+})
+
+const server = http
+  .createServer((req, res) => router.handleRequest(req, res))
+  .listen(PORT)
